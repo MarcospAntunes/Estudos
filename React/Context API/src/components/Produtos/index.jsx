b73e6c6b5703bee70f2b288fdb97d263c746f74b
Marcospantunes <marcospantunes203@gmail.com>
2024-02-04 10:45:48 -0300

    Context API
    Inicio do estudo de Context API e criação do primeiro contexto.

---
+++ React/Context API/src/components/Produtos/index.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+import Produto from "./Produto";
+import produtos from "@/mocks/produtos.json";
+import Titulo from "@/components/Titulo";
+import { CarrinhoContext } from "@/context/CarrinhoContext";
+
+const Produtos = () => {
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+  function adicionarProduto (novoProduto) {
+    const temOproduto = carrinho.some((itemDoCarrinho) => { itemDoCarrinho.id === novoProduto.id});
+
+    if(!temOproduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho((prev) => [
+        ...prev,
+        novoProduto
+      ]);
+    }
+
+    setCarrinho((prev) => prev.map((itemDoCarrinho) => {
+      if(itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
+      return itemDoCarrinho
+    }))
+  }
+
+  return (
+    <section role="produtos" aria-label="Produtos que estão bombando!">
+      <Titulo>Produtos que estão bombando!</Titulo>
+      <div className="container row mx-auto">
+        {produtos.map((produto) => (
+          <Produto
+            key={produto.id}
+            {...produto}
+            adicionarProduto={adicionarProduto}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Produtos;
