b3fc91727478808f744f5deda15f48d7fcf9f748
Marcospantunes <marcospantunes203@gmail.com>
2024-02-12 06:29:13 -0300

    getServerSideProps
    getServerSideProps roda a cada acesso recebido na página. Ela deve ser usada no lugar de getStaticProps caso seja necessário que a informação sempre se atualize.

--- NextJs/pages/faq.jsx
+++ NextJs/pages/faq.jsx
@@ -1,5 +1,14 @@
 import Link from "next/link";
 
+// SSG - Static Site Generation
+// SSR - Server Side Rendering
+// ISG - Incremental Static Generation
+
+// -> getServerSideProps roda a cada acesso recebido na página.
+// -> getStaticProps roda somente em build time.
+// -> Porém, em modo desenvolvimento, ambas sempre iram rodar.
+
+
 export async function getStaticProps() {
     const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
     const res = await fetch(FAQ_API_URL);
@@ -14,7 +23,6 @@ export async function getStaticProps() {
 }
 
 export default function FAQPage({ data }) {
-
     return(
         <div>
             <h1>FAQ</h1>
