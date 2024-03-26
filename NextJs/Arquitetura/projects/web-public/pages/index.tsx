4d01b1348302938ad9bc811c210025e5cd70c61e
Marcospantunes <marcospantunes203@gmail.com>
2024-03-26 17:13:20 -0300

    feat: Workspaces e o pacote utils

--- NextJs/Arquitetura/projects/web-public/pages/index.tsx
+++ NextJs/Arquitetura/projects/web-public/pages/index.tsx
@@ -1,9 +1,11 @@
-import react from 'react'
+import react from 'react';
+import { sum } from '@alura/utils/math/sum';
 
 function HomeScreen() {
     return(
         <main>
             <h1>Home</h1>
+            <p>Importando modulo local: @alura/utils/math/sum sum(2,2): {sum(2,2)}</p>
         </main>
     )
 }
