c9d24741bda40d14a5913ec1fedea3c61c79d026
Marcospantunes <marcospantunes203@gmail.com>
2024-03-27 18:08:05 -0300

    Adicionando e configurando o ESLint

--- NextJs/Arquitetura/projects/web-public/pages/index.tsx
+++ NextJs/Arquitetura/projects/web-public/pages/index.tsx
@@ -1,14 +1,15 @@
-import { Text } from '@alura/design-system/components/Text';
-import { sum } from '@alura/utils/math/sum';
+import { Text } from "@alura/design-system/components/Text";
+import { sum } from "@alura/utils/math/sum";
 
 function HomeScreen() {
-    return(
-        <main>
-            <Text tag="h1">Home</Text>
-            <Text tag="p">Importando modulo local: @alura/utils/math/sum sum(2,2): {sum(2,2)}</Text>
-        </main>
-    )
+  return (
+    <main>
+      <Text tag="h1">Home</Text>
+      <Text tag="p">
+        Importando modulo local: @alura/utils/math/sum sum(2,2): {sum(2, 2)}
+      </Text>
+    </main>
+  );
 }
-
 
 export default HomeScreen;
